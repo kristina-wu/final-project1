7040958565a0ee7a540ac84d5d13218e1fdc1fa1
kristina-wu <812315376@qq.com>
2017-05-29 20:39:05 +0800

    Update queue.h

--- include/queue.h
+++ include/queue.h
@@ -2,7 +2,9 @@
 #define _ARRAY_H
 sequeue_t *CreateEmptySequeue();
 int EmptySequeue(sequeue_t *queue);
-int EnQueue(sequeue_t *queue, data_t x);
-int DeQueue(sequeue_t *queue, data_t *x);
+int EnQueue(sequeue_t *queue, int x);
+int DeQueue(sequeue_t *queue, int *x);
+int FullSequeue(sequeue_t *queue);
+void DestroySequeue(sequeue_t *queue);
 
 #endif
