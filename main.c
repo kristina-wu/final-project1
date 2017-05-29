00ad640f3c60e834ed80b5724c32d9cb32304e48
kristina-wu <812315376@qq.com>
2017-05-29 20:37:03 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,52 +1,45 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<string.h>
-#include"include/create queue.h"
-#include"include/empty queue.h"
-#include"include/enqueue.h"
-#include"include/dequeue.h"
-#include"include/create stack.h"
-#include"include/empty stack.h"
-#include"include/push stack.h"
-#include"include/pop.h"
-#include"include/inittree.h"
-#include"include/insert tree.h"
-#include"include/delete tree.h"
-#include"include/travel tree.h"
-#include"include/tree depth.h"
-#define N 64  //队列中数据元素的数据类型  
-typedef struct  
-{  
-    int a[N]; //用数组作为队列的储存空间  
-    int front,rear; //指示队头位置和队尾位置的指针  
+
+#include"include/queue.h"
+#include"include/stack.h"
+#include"include/tree.h"
+
+#define N 64  //队列中数据元素的数据类型
+typedef struct
+{
+    int a[N]; //用数组作为队列的储存空间
+    int front,rear; //指示队头位置和队尾位置的指针
 }sequeue_t;
 typedef struct node{
     int b;
     struct node * next;
 }linknode,*linkstack;
-typedef struct CSNode{
-      char c;
-      struct SCNode *firstchild,*nextsibling;
-}TNode,*Tree;
+typedef struct BitNode
+{
+    int c;
+    struct BitNode *lchild,*rchild;
+}BitNode,*BitTree;
 
 int main()
 {
-   int p,*q;int t;
-   scanf("%d",&p);
-   sqeueue_t *queue;
-   queue=CreateEmptySequeue();
-   t=EmptySequeue(*queue); printf("%d",t);
-   EnQueue(*queue, p);
-   DeQueue(*queue, *q);
-   linkstack h;
-   h=linkstack create();
-   t=isempty(h); printf("%d",t);
-   push(h,p);
-   t=pop(h);printf("%d",t);
-   Tree T;
-   T=InitTree();
-   TraverseTree(T);
-   TreeDepth(T);
-   deleteTree(T);
-   return 0;  
+    int p,*q;int t,d;
+    scanf("%d",&p);
+    sequeue_t *queue;
+    queue=CreateEmptySequeue();
+    t=EmptySequeue(queue); printf("%d",t);
+    EnQueue(queue, p);
+    DeQueue(queue, q);
+    linkstack h;
+    h=create();
+    t=isempty(h); printf("%d",t);
+    push(h,p);
+    t=pop(h);printf("%d",t);
+    BitTree T;
+    T=BinTreeCreat(T);
+    t=BinTreeEmpty(T);printf("%d",t);
+    d=BinTreeDepth(T);
+    PreOrderTraverse(T);
+    BinTreeClear(&T);
+    return 0;
 }
